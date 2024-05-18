20bfe0d4b73fe8bc574f2ecb5eae598333bf641c
shallondev <shallon.frances.pawlickpotts@gmail.com>
2024-05-18 14:25:22 -0400

    la

--- beginner/script.js
+++ beginner/script.js
@@ -1,2 +1,17 @@
 // Search for HTML element canvas
 var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+// Line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "#f9a232";
+c.stroke();
+
+console.log(canvas);
